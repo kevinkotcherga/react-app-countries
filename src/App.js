a8f79960b6a9cb52d47ec8bd78a50c7375427ea2
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-27 14:47:11 +0100

    ✨feat(style) modification du style home

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/a-propos" element={<About/>} />
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<Home/>}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,12 +4,18 @@ import { NavLink } from 'react-router-dom'
 const Navigation = () => {
   return (
     <div className='navigation'>
-      <NavLink exact to="/">
-        Accueil
-      </NavLink>
-      <NavLink exact to="a-propos">
-        A propos
-      </NavLink>
+      <nav>
+        <ul>
+          <NavLink to="/"
+          className={(nav) => (nav.isActive ? "nav-active" : "")}>
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink to="/a-propos"
+          className={(nav) => (nav.isActive ? "nav-active" : "")}>
+            <li>A propos</li>
+          </NavLink>
+        </ul>
+      </nav>
     </div>
   );
 };
